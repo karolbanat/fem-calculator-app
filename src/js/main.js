8c09a0d86f0890d0495b44d0e3d78636e1c62816
Karol Banat <karol.banat@gmail.com>
2022-11-08 17:04:15 +0100

    Basic calculator functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 const themeSwitcherButtons = document.querySelectorAll('.theme-switcher__radio-btn');
+/* caluculator elements */
+const calculator = document.querySelector('.calculator');
+const valueKeys = calculator.querySelectorAll('button[data-value]');
+const operationKeys = calculator.querySelectorAll('button[data-operation]');
+const calculatorOutput = calculator.querySelector('#calculator-output');
 
 /* theme handling */
 const loadTheme = () => {
@@ -29,9 +34,111 @@ const checkRadioBtn = theme => {
 		if (btn.value === theme) btn.checked = true;
 	});
 };
+themeSwitcherButtons.forEach(btn => btn.addEventListener('change', e => changeTheme(e.target.value)));
 /* theme handling end */
 
-/* event listeners */
-themeSwitcherButtons.forEach(btn => btn.addEventListener('change', e => changeTheme(e.target.value)));
+/* calculator handling */
+class Calculator {
+	result = 0.0;
+	operation = undefined;
+	buffer = '0';
+
+	invokeOperation(nextOperation = undefined) {
+		switch (this.operation) {
+			case '+':
+				this.result = this.result + parseFloat(this.buffer);
+				break;
+			case '-':
+				this.result = this.result - parseFloat(this.buffer);
+				break;
+			case '/':
+				this.result = this.result / parseFloat(this.buffer);
+				break;
+			case '*':
+				this.result = this.result * parseFloat(this.buffer);
+				break;
+			default:
+				this.result = parseFloat(this.buffer);
+		}
+
+		this.setOperation(nextOperation);
+		this.buffer = '0';
+	}
+
+	setOperation(operation) {
+		this.operation = operation;
+	}
+
+	reset() {
+		this.buffer = '0';
+		this.result = 0.0;
+		this.operation = undefined;
+	}
+
+	increaseValue(value) {
+		if (value === '.' && this.buffer.includes('.')) return;
+
+		this.buffer += value;
+		this.transformLead();
+	}
+
+	decreaseValue() {
+		this.buffer = this.buffer.slice(0, -1) || '0';
+	}
+
+	transformLead() {
+		this.trimLeadingZeros();
+		this.convertLeadingDot();
+	}
+
+	trimLeadingZeros() {
+		/* remove leading zeros */
+		this.buffer = this.buffer.replace(/^0+/, '') || '0';
+	}
+
+	convertLeadingDot() {
+		/* if there is dot at the start, replace it with 0. */
+		this.buffer = this.buffer.replace(/^\./, '0.');
+	}
+}
+
+const calc = new Calculator();
+
+const handleOperationKey = e => {
+	const operation = e.target.dataset.operation;
+	switch (operation) {
+		case 'delete':
+			calc.decreaseValue();
+			displayOutput(calc.buffer);
+			break;
+		case 'reset':
+			calc.reset();
+			displayOutput(calc.buffer);
+			break;
+		case '+':
+		case '-':
+		case '/':
+		case '*':
+			calc.invokeOperation(operation);
+			displayOutput(calc.buffer);
+			break;
+		case '=':
+			calc.invokeOperation();
+			displayOutput(calc.result);
+			break;
+	}
+};
+const displayOutput = (value = '0') => {
+	calculatorOutput.innerText = value;
+};
+
+valueKeys.forEach(key =>
+	key.addEventListener('click', e => {
+		calc.increaseValue(e.target.dataset.value);
+		displayOutput(calc.buffer);
+	})
+);
+
+operationKeys.forEach(key => key.addEventListener('click', handleOperationKey));
 
 loadTheme();
